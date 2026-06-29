6a846c6087c00c93e5f27c84ed88761102e3e6da
rudransh goswami <rudranshgoswami415@gmail.com>
2026-06-29 23:54:09 +0530

    Added the current function

--- omw_law.py
+++ omw_law.py
@@ -1,6 +1,6 @@
 # Simulating oms law in python 
 
-# The three formulas are :
+# The three formula are :
 # Find Voltage: V = I × R
 # Find Current: I = V/R
 # Find Resistance: R = V/I
@@ -11,12 +11,23 @@
 name = input('Enter your name : ')
 print(f'Welcome Dear {name}')
 
-current = float(input('enter the exact current flow '))
-resistance = float(input('enter the exact resistance flow '))
 
-def formula():
+def volt():
+    current = float(input('enter the exact current flow '))
+    resistance = float(input('enter the exact resistance flow '))
     print(f'Your current is {current}:amps , and resistance is {resistance}:omw')
     voltage = current * resistance
     print(f'Your Voltage is {voltage}volt')
     
-formula()
+volt()
+
+
+
+def Current():
+    voltage = float(input('enter the exact voltage '))
+    resistance = float(input('enter the exact current '))
+    print(f'your Current voltage is {voltage}volt , and current is {resistance}omw ') 
+    current = voltage / resistance
+    print(f'your Current is {current}amps') 
+
+Current()
